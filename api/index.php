a31821200e8aaa44d94870a50139d196167facfa
Fareltandyawan-ua <farel.tandyawan-2024@vokasi.unair.ac.id>
2026-07-09 11:19:03 +0700

    Fix Vercel Laravel deploy

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
 
 require __DIR__ . '/../public/index.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Tambahkan kode ini untuk mengarahkan storage ke folder /tmp Vercel
+$app->useStoragePath($_ENV['APP_STORAGE'] ?? '/tmp/storage');
